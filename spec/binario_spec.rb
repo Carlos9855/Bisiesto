422865f782a72e76f70b88324aa4c8e4057e8415
carlos camacho <josecarloscamacho55@gmail.com>
2021-04-26 11:54:47 -0400

    Cuarta prueba se añadio 2 pruebas mas

--- spec/binario_spec.rb
+++ spec/binario_spec.rb
@@ -12,4 +12,12 @@ RSpec.describe "bisiesto" do
     it "deberia devolver false para el año 2021" do
         expect(es_bisiesto(2021)).to eq(false)
     end
+
+    it "deberia devolver true para el año 1984" do
+        expect(es_bisiesto(1984)).to eq(true)
+    end
+
+    it "deberia devolver false para el año 589" do
+        expect(es_bisiesto(589)).to eq(false)
+    end
 end 
